2fc01e5ab4bbbef0d4e52a4ce188f68c813f94f0
Stijn Verdenius <stijnverdenius@pop-os.localdomain>
2019-01-17 16:46:52 +0100

    rformat and added readme first message

--- development_code/main.py
+++ development_code/main.py
@@ -1,5 +1,3 @@
-
-
 import numpy as np
 from saver import Saver
 from step1 import Step1
@@ -9,23 +7,19 @@ from step4 import Step4
 from step5 import Step5
 from step6 import Step6
 
-
 save_and_load = Saver("data/")
 
-
-
 ## Step 0 : Loading data
-
 
 data = save_and_load.load_data_model_1()
 
 steps = [
-Step1(1, "Simulate Rankings of Relevance for E and P", data),
-Step2(2, "Calculate the 𝛥measure", data),
-Step3(3, "Implement Team-Draft Interleaving and Probabilistic Interleaving ", data),
-Step4(4, "Simulate User Clicks", data),
-Step5(5, "Simulate Interleaving Experiment", data),
-Step6(6, "Compute Sample Size", data),
+    Step1(1, "Simulate Rankings of Relevance for E and P", data),
+    Step2(2, "Calculate the 𝛥measure", data),
+    Step3(3, "Implement Team-Draft Interleaving and Probabilistic Interleaving ", data),
+    Step4(4, "Simulate User Clicks", data),
+    Step5(5, "Simulate Interleaving Experiment", data),
+    Step6(6, "Compute Sample Size", data),
 ]
 
 counter = 0
@@ -37,34 +31,25 @@ def do_next_step(input_list, counter):
 
 ## Step 1: Simulate Rankings of Relevance for E and P
 
-
 some_output, counter = do_next_step(["some input", "other input"], counter)
-
 
 ## Step 2: Calculate the 𝛥measure
 
-
-
 some_output, counter = do_next_step(["some input", "other input"], counter)
 
-
 ## Step 3: Implement Team-Draft Interleaving (5pts) and Probabilistic Interleaving (35 points)
-
 
 some_output, counter = do_next_step(["some input", "other input"], counter)
 
 ## Step 4: Simulate User Clicks (40 points)
 
-
 some_output, counter = do_next_step(["some input", "other input"], counter)
 
 ## Step 5: Simulate Interleaving Experiment
 
-
 some_output, counter = do_next_step(["some input", "other input"], counter)
 
 ## Step 6: Compute Sample Size
-
 
 some_output, counter = do_next_step(["some input", "other input"], counter)
 
