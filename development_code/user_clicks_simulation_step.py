521d3a91c23ad6f03bb2698a1d364ff542ec0d12
Unknown <Kassapiselias@hotmail.co.uk>
2019-01-20 19:06:06 +0100

    latest state of q4

--- development_code/user_clicks_simulation_step.py
+++ development_code/user_clicks_simulation_step.py
@@ -61,21 +61,68 @@ for line in f:
 # print(qs.values())
 
 
+# alphas = {} # key = document, value = query : a_uq
+# for f in frame:
+#     if f['TypeOfAction'] == 'Q':
+#         for u in f["ListOfURLs"]:
+#             if u not in alphas.keys():
+#                 alphas[u] = {f['QueryID']:0.5}
+#             if u in alphas.keys():
+#                 if f['QueryID'] not in alphas[u].keys():
+#                     alphas[u][f['QueryID']] = 0.5
 
-# uq = {} #key = document url, value = Query id: list of sessions
-# for i in frame:
-#     if i['TypeOfAction'] == 'Q':
-#         for u in i['ListOfURLs']:
-#             if u not in uq.keys():
-#                 uq[u] = {i['QueryID'] : [i['SessionID']]} # add url and corresponding query and session id
-#             if u in uq.keys():
-#                 if i['QueryID'] in uq[u].keys():          #check if document vs. query combo already exists
-#                     if i['SessionID'] not in uq[u][i['QueryID']]: #check if session id already exists in document vs. query combo
-#                         uq[u][i['QueryID']].append(i['SessionID'])
+
+
+uq = {} #key = document url, value = Query id: list of sessions
+for i in frame:
+    if i['TypeOfAction'] == 'Q':
+        for u in i['ListOfURLs']:
+            if u not in uq.keys():
+                uq[u] = {i['QueryID'] : [i['SessionID']]} # add url and corresponding query and session id
+            if u in uq.keys():
+                if i['QueryID'] in uq[u].keys():          #check if document vs. query combo already exists
+                    if i['SessionID'] not in uq[u][i['QueryID']]: #check if session id already exists in document vs. query combo
+                        uq[u][i['QueryID']].append(i['SessionID'])
+                else:
+                    uq[u][i['QueryID']] = [i['SessionID']]
+
+# print(uq[17560])
+
+
+
+
+
+sc = {}
+sc_id = {}
+for i in frame:
+    if i["SessionID"] not in sc.keys():
+        sc[i['SessionID']] = [] #Empty if session does not result in click
+        sc_id[i['SessionID']] = []
+    if i['TypeOfAction'] == 'Q':
+        current_q = {"SessionID": i["SessionID"], "ListOfURLs": i["ListOfURLs"][:6]}
+    if i['TypeOfAction'] == 'C':
+        for e in enumerate(current_q["ListOfURLs"]):
+            if e[1] == i["URLID"]:
+                rank = e[0]+1
+                sc_id[i['SessionID']].append(i["URLID"])
+        sc[i['SessionID']].append(rank) #The rank of the url id clicked in the given session
+
+print(sc[0])
+print(sc_id[0])
+
+
+
+# for document in uq:
+#     for query in uq[document]:
+#         for session in uq[document][query]:
+#             # print(session)
+#             if session in sc_id.keys():
+#                 if document in sc_id[session]:
+#                     click = 1
 #                 else:
-#                     uq[u][i['QueryID']] = [i['SessionID']]
-#
-# # print(uq[100])
+#                     click = 0
+
+
 #
 #
 # sc = {} #key = Session ID, value = clicked rank
@@ -137,37 +184,99 @@ def get_uq(data):
 
 
 
-def get_clicks_per_session(data):
+
+def get_sc(data):
     """
     :param data - in the form of a list of libraries
     :return - library where key = Session ID, value = clicked rank
+            & library where key = Session ID, value = clicked id
     """
     sc = {}
+    sc_id = {}
     for i in data:
         if i["SessionID"] not in sc.keys():
             sc[i['SessionID']] = [] #Empty if session does not result in click
+            sc_id[i['SessionID']] = []
         if i['TypeOfAction'] == 'Q':
-            current_q = {"SessionID": i["SessionID"], "ListOfURLs": i["ListOfURLs"][:6]}
+            current_q = {"SessionID": i["SessionID"], "ListOfURLs": i["ListOfURLs"][:6]} # may need to remove :6!!!!!!!!!
         if i['TypeOfAction'] == 'C':
             for e in enumerate(current_q["ListOfURLs"]):
                 if e[1] == i["URLID"]:
                     rank = e[0]+1
+                    sc_id[i['SessionID']].append(i["URLID"])
             sc[i['SessionID']].append(rank) #The rank of the url id clicked in the given session
-    return sc
-
-
-# def EMiter():
-#     # epsilon =  1e-6
-#     alphas = []
-#     gammas = []
-#     for i in
+    return sc, sc_id
 
 
 
 
+def init_alphas(data):
+    """
+    :param data -  in the form of a list of libraries
+    :return - library where key = document, value = query : a_uq
+    """
+    alphas = {} # key = document, value = query : a_uq
+    for f in frame:
+        if f['TypeOfAction'] == 'Q':
+            for u in f["ListOfURLs"]:
+                if u not in alphas.keys():
+                    alphas[u] = {f['QueryID']:0.5}
+                if u in alphas.keys():
+                    if f['QueryID'] not in alphas[u].keys():
+                        alphas[u][f['QueryID']] = 0.5
+    return alphas
 
 
 
-# def EMtrain():
+
+def alpha_update(alphas, gammas, uq, sc_id, sc):
+    """
+    :return - update by iterating though all query vs. document seshs
+    """
+    for document in uq:
+        for query in uq[document]:
+            counter = 0
+            for session in uq[document][query]:
+                counter += 1
+                if session in sc_id.keys():
+                    if document in sc_id[session]: #gives click for session of u vs. q combo
+                        click = 1
+                    else:
+                        click = 0
+
+                for i,d in enumerate(sc_id[session]): # get corresponding rank of document clicked
+                    if d == document:
+                        rank = sc[session][i]
+
+                fraction = ((1 - gammas[rank])*alphas[document][query])/(1 - (gammas[rank]*alphas[document][query])) # check alphas[document][query]
+
+                alphas[document][query] += (click + (1-click)*(fraction))
+            alphas[document][query] /= counter
+    return alphas
+
+
+
+
+def gamma_update(alphas, gammas):
+    for r in
+
+
+
+
+
+
+
+# def EMiter(data):
+#     uq = get_uq(data) #change frame to whatever is the data saved as
+#     sc, sc_id = get_sc(data)
+#     alphas = init_alphas(data) # initializing first alpha
+#     gammas = [1, 0.8, 0.6, 0.4, 0.2, 0.0] #initializing gammas
+
+
+
 #
-#     convergence_eps = 0.01
+# def EMtrain(data):
+#
+#
+
+
