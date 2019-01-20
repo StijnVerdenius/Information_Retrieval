9401c30bf43622840ae1ac2398877205b4ae199d
Unknown <Kassapiselias@hotmail.co.uk>
2019-01-20 14:24:21 +0100

    current state of q4 - actual

--- development_code/user_clicks_simulation_step.py
+++ development_code/user_clicks_simulation_step.py
@@ -48,77 +48,92 @@ for line in f:
 
 
 
-qs = {} #key = Query ID, value = list of libraries with key - Session ID, value - List of URLs
-
-for i in frame:
-    if i['TypeOfAction'] == 'Q':
-        current_s = {i['SessionID']:i['ListOfURLs']}
-        if i['QueryID'] not in qs.keys():
-            qs[i['QueryID']] = [current_s]
-        elif i['QueryID'] in qs.keys():
-            qs[i['QueryID']].append(current_s)
-
-print(qs.values())
-
-
-uq = {}
-for i in frame:
-    if i['TypeOfAction'] == 'Q':
-        current_q = {i['QueryID'] : i['SessionID']} #Key = Query, Value = Session ---> current_q[Query_id] = session_id
-        for u in i['ListOfURLs']:
-            if u not in uq.keys():
-                uq[u] = {i['QueryID'] : [i['SessionID']]}
-            if u in uq.keys():
-                if i['QueryID'] in uq[u].keys():
-                    if i['SessionID'] not in uq[u][i['QueryID']]:
-                        uq[u][i['QueryID']].append(i['SessionID'])
+# qs = {} #key = Query ID, value = list of libraries with key - Session ID, value - List of URLs
+#
+# for i in frame:
+#     if i['TypeOfAction'] == 'Q':
+#         current_s = {i['SessionID']:i['ListOfURLs']}
+#         if i['QueryID'] not in qs.keys():
+#             qs[i['QueryID']] = [current_s]
+#         elif i['QueryID'] in qs.keys():
+#             qs[i['QueryID']].append(current_s)
+#
+# print(qs.values())
 
 
 
-
-
-
-
-
-
-
-
-sc = {} #key = Session ID, value = clicked rank
-
-for i in frame:
-    if i["SessionID"] not in sc.keys():
-        sc[i['SessionID']] = [] #Empty if session does not result in click
-
-    if i['TypeOfAction'] == 'Q':
-        current_q = {"SessionID": i["SessionID"], "ListOfURLs": i["ListOfURLs"][:6]}
-
-    if i['TypeOfAction'] == 'C':
-
-        for e in enumerate(current_q["ListOfURLs"]):
-            if e[1] == i["URLID"]:
-                rank = e[0]+1
-
-        sc[i['SessionID']].append(rank) #The rank of the url id clicked in the given session
+# uq = {} #key = document url, value = Query id: list of sessions
+# for i in frame:
+#     if i['TypeOfAction'] == 'Q':
+#         for u in i['ListOfURLs']:
+#             if u not in uq.keys():
+#                 uq[u] = {i['QueryID'] : [i['SessionID']]} # add url and corresponding query and session id
+#             if u in uq.keys():
+#                 if i['QueryID'] in uq[u].keys():          #check if document vs. query combo already exists
+#                     if i['SessionID'] not in uq[u][i['QueryID']]: #check if session id already exists in document vs. query combo
+#                         uq[u][i['QueryID']].append(i['SessionID'])
+#                 else:
+#                     uq[u][i['QueryID']] = [i['SessionID']]
+#
+# # print(uq[100])
+#
+#
+# sc = {} #key = Session ID, value = clicked rank
+#
+# for i in frame:
+#     if i["SessionID"] not in sc.keys():
+#         sc[i['SessionID']] = [] #Empty if session does not result in click
+#
+#     if i['TypeOfAction'] == 'Q':
+#         current_q = {"SessionID": i["SessionID"], "ListOfURLs": i["ListOfURLs"][:6]}
+#
+#     if i['TypeOfAction'] == 'C':
+#
+#         for e in enumerate(current_q["ListOfURLs"]):
+#             if e[1] == i["URLID"]:
+#                 rank = e[0]+1
+#
+#         sc[i['SessionID']].append(rank) #The rank of the url id clicked in the given session
 
 
 # print(len(list(sc.keys())),'sc should be 11717')
 
 ###############################################################################################
 
-def get_sessions_per_query(data):
+# def get_sessions_per_query(data):
+#     """
+#     :param data - in the form of a list of libraries
+#     :return - library where key = Query ID, value = (Session IDs, list of URLs)
+#     """
+#     qs = {}
+#     for i in frame:
+#         if i['TypeOfAction'] == 'Q':
+#             if i['QueryID'] not in qs.keys():
+#                 qs[i['QueryID']] = ([i['SessionID']], i['ListOfURLs'])
+#             elif i['QueryID'] in qs.keys():
+#                 qs[i['QueryID']][0].append(i['SessionID'])
+#     return qs
+
+
+def get_uq(data):
     """
     :param data - in the form of a list of libraries
-    :return - library where key = Query ID, value = (Session IDs, list of URLs)
+    :return - library with key = document url, value = Query id: list of sessions
     """
-    qs = {}
+    uq = {}
     for i in frame:
         if i['TypeOfAction'] == 'Q':
-            if i['QueryID'] not in qs.keys():
-                qs[i['QueryID']] = ([i['SessionID']], i['ListOfURLs'])
-            elif i['QueryID'] in qs.keys():
-                qs[i['QueryID']][0].append(i['SessionID'])
-    return qs
+            for u in i['ListOfURLs']:
+                if u not in uq.keys():
+                    uq[u] = {i['QueryID'] : [i['SessionID']]} # add url and corresponding query and session id
+                if u in uq.keys():
+                    if i['QueryID'] in uq[u].keys():          #check if document vs. query combo already exists
+                        if i['SessionID'] not in uq[u][i['QueryID']]: #check if session id already exists in document vs. query combo
+                            uq[u][i['QueryID']].append(i['SessionID'])
+                    else:
+                        uq[u][i['QueryID']] = [i['SessionID']]
 
+    return uq
 
 
 
@@ -139,7 +154,6 @@ def get_clicks_per_session(data):
                     rank = e[0]+1
             sc[i['SessionID']].append(rank) #The rank of the url id clicked in the given session
     return sc
-
 
 
 # def EMiter():
